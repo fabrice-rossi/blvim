8382323bbc6677799bfd7d39f424f3a1ac0c60f7
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-03-06 19:32:16 +0100

    Faster version of the static model.

--- src/wilson_entropy.cpp
+++ src/wilson_entropy.cpp
@@ -9,14 +9,7 @@
 arma::mat ll_we_oc(const arma::mat& exp_beta_costs,
                    const arma::vec& X,
                    const arma::vec& Z_alpha) {
-  arma::mat Y(exp_beta_costs.n_rows, exp_beta_costs.n_cols);
-  arma::vec A = exp_beta_costs * Z_alpha;
-  for(uint j = 0; j < Y.n_cols; j++) {
-    for(uint i = 0; i < Y.n_rows; i++) {
-      Y(i, j) = X.at(i) * Z_alpha.at(j) * exp_beta_costs.at(i, j) / A.at(i);
-    }
-  }
-  return Y;
+  return ((X/(exp_beta_costs * Z_alpha)) * Z_alpha.t()) % exp_beta_costs;
 }
 
 // [[Rcpp::export]]
@@ -29,3 +22,4 @@ arma::mat we_oc(const arma::mat& costs,
   arma::vec Z_alpha = arma::pow(Z, alpha);
   return ll_we_oc(exp_beta_costs, X, Z_alpha);
 }
+
