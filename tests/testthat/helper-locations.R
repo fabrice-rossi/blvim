ad779e68645753bd6d66117295d49c5ea75186cf
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-03-07 12:44:47 +0100

    Remove dimnames to ease testing.

--- tests/testthat/helper-locations.R
+++ tests/testthat/helper-locations.R
@@ -3,6 +3,7 @@ create_locations <- function(nprod, ndest, seed = 0) {
   pos_prod <- matrix(rnorm(nprod * 2), ncol = 2)
   pos_dest <- matrix(rnorm(ndest * 2), ncol = 2)
   distances <- as.matrix(dist(rbind(pos_prod, pos_dest)))[1:nprod, (nprod + 1):(nprod + ndest)]
+  dimnames(distances) <- NULL
   production <- runif(nrow(pos_prod), min = 0.1, max = 2)
   attractiveness <- runif(nrow(pos_dest), min = 1, max = 2)
   list(costs = distances, X = production, Z = attractiveness, pp = pos_prod, pd = pos_dest)
