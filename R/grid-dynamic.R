81a2877de265c705543aaa9de05fd804756b375a
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-04-20 15:49:15 +0200

    Add an optional progress bar.

--- R/grid-dynamic.R
+++ R/grid-dynamic.R
@@ -1,16 +1,17 @@
 #' Compute a collection of Boltzmann–Lotka–Volterra model solutions
 #'
-#' This function computes a collection of flows between origin locations
-#' and destination locations using [blvim()] on a grid of parameters. The flows
-#' use the same costs, same production constraints and same attractivenesses.
-#' Each flow is computed using one of all the pairwise combinations between
-#' the alpha values given by `alphas` and the beta values given by `betas`. The
-#' function returns an object of class `sim_list` which contains the resulting
-#' flows.
+#' This function computes a collection of flows between origin locations and
+#' destination locations using [blvim()] on a grid of parameters. The flows use
+#' the same costs, same production constraints and same attractivenesses. Each
+#' flow is computed using one of all the pairwise combinations between the alpha
+#' values given by `alphas` and the beta values given by `betas`. The function
+#' returns an object of class `sim_list` which contains the resulting flows.
 #'
 #' @inheritParams blvim
 #' @param alphas a vector of return to scale parameters
 #' @param betas a vector of cost inverse scale parameters
+#' @param progress if TRUE, a progress bar is shown during the calculation
+#'   (defaults to FALSE)
 #' @return an object of class `sim_list`
 #' @export
 #' @examples
@@ -27,9 +28,13 @@ grid_blvim <- function(costs, X, alphas, betas, Z,
                        iter_max = 50000,
                        conv_check = 100,
                        precision = 1e-6,
-                       quadratic = FALSE) {
+                       quadratic = FALSE,
+                       progress = FALSE) {
   params <- expand.grid(alpha = alphas, beta = betas)
   all_models <- vector(mode = "list", length = nrow(params))
+  if (progress) { # nocov start
+    cli::cli_progress_bar("Computing models", total = length(all_models))
+  } # nocov end
   for (k in seq_along(all_models)) {
     all_models[[k]] <- blvim(
       costs, X,
@@ -41,6 +46,12 @@ grid_blvim <- function(costs, X, alphas, betas, Z,
       precision = precision,
       quadratic = quadratic
     )
+    if (progress) { # nocov start
+      cli::cli_progress_update()
+    } # nocov end
   }
+  if (progress) { # nocov start
+    cli::cli_progress_done()
+  } # nocov end
   new_sim_list(all_models)
 }
