3980fe83a796596a2e6696f8dc25dce882972be1
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-06-05 19:00:05 +0200

    Add a sim data frame.
    The sim_df object is inspired by the sf object in the sf package.

---
+++ R/sim_df.R
@@ -0,0 +1,87 @@
+new_sim_df <- function(sim_list, ..., class = character()) {
+  if (!inherits(sim_list, "sim_list")) {
+    cli::cli_abort("{.arg sim_list} must be a {.cls sim_list}")
+  }
+  iterations <- sapply(sim_list, sim_iterations)
+  converged <- sapply(sim_list, sim_converged)
+  pre_result <- data.frame(
+    alpha = sim_list$alphas,
+    beta = sim_list$betas,
+    diversity = sapply(sim_list, diversity),
+    iterations = iterations,
+    converged = converged,
+    sim = I(sim_list)
+  )
+  class(pre_result) <- c(class, "sim_df", class(pre_result))
+  pre_result
+}
+
+#' Create a spatial interaction data frame from a collection of interaction models
+#'
+#' This function build a data frame from a collection of spatial interaction
+#' model. The data frame has a list column `sim` which stores the collection
+#' of models and classical columns that contain characteristics of the models.
+#' See details for the default columns.
+#'
+#' The data frame has one row per spatial interaction model and the following columns:
+#' - `sim`: the last column that contains the model
+#' - `alpha`: the return to scale parameter used to build the model
+#' - `beta`: the cost inverse scale parameter used to build the model
+#' - `diversity`: model default [diversity()] (Shannon's diversity)
+#' - `iterations`: the number of iterations used to produce the model (1 for a
+#'    static model)
+#' - `converged`: `TRUE` is the iterative calculation of the model converged (for
+#'    models produced by [blvim()] and related approaches), `FALSE` for no convergence
+#'    and `NA` for static models
+#'
+#'
+#' @param x a collection of spatial interaction models, an object of class
+#'   `sim_list`
+#'
+#' @returns a data frame representation of the spatial interaction model collection
+#'  with classes `sim_df` and `data.frame`
+#' @export
+#' @examples
+#' positions <- matrix(rnorm(10 * 2), ncol = 2)
+#' distances <- as.matrix(dist(positions))
+#' production <- rep(1, 10)
+#' attractiveness <- c(2, rep(1, 9))
+#' all_flows <- grid_blvim(distances, production, c(1.25, 1.5), c(1, 2, 3), attractiveness)
+#' all_flows_df <- sim_df(all_flows)
+#' all_flows_df$converged
+#'
+sim_df <- function(x) {
+  new_sim_df(x)
+}
+
+#' @export
+`$<-.sim_df` <- function(x, name, value) {
+  if (name == "sim") {
+    if (is.null(value) || !inherits(value, "sim_list")) {
+      x[[name]] <- value
+      ## remove the sim_df class
+      class(x) <- setdiff(class(x), "sim_df")
+      return(x)
+    } else if (inherits(value, "sim_list")) {
+      x[[name]] <- I(value)
+      return(x)
+    }
+    ## sim_list replacement
+  }
+  x[[name]] <- value
+  x
+}
+
+#' @export
+`[.sim_df` <- function(x, i, ...) {
+  ## if ... is empty and i does not contain sim, we have to remove the sim_df
+  ## class.
+  pre <- NextMethod()
+  if (inherits(pre, "sim_df")) {
+    if (!"sim" %in% names(pre)) {
+      class(pre) <- setdiff(class(pre), "sim_df")
+      return(pre)
+    }
+  }
+  pre
+}
---
+++ tests/testthat/test-sim_df.R
@@ -0,0 +1,79 @@
+test_that("sim_df constructor error detection", {
+  expect_error(sim_df(1:5))
+})
+
+test_that("sim_df creation obeys to its documentation", {
+  config <- create_locations(25, 20, seed = 64)
+  alphas <- seq(1.25, 2, by = 0.25)
+  betas <- 1 / seq(0.05, 0.5, length.out = 4)
+  models <- grid_blvim(config$costs,
+    config$X,
+    alphas,
+    betas,
+    config$Z,
+    iter_max = 10000,
+    precision = .Machine$double.eps^0.5
+  )
+  models_df <- sim_df(models)
+  ## classes
+  expect_s3_class(models_df, "data.frame")
+  expect_s3_class(models_df, "sim_df")
+  ## column names
+  expect_named(models_df, c("alpha", "beta", "diversity", "iterations", "converged", "sim"), ignore.order = TRUE)
+  ## sim is the last one
+  expect_equal(names(models_df)[6], "sim")
+  ## models (low level, maybe implemented in a more abstract way)
+  expect_equal(models_df$sim$sims, models$sims)
+  ## parameters
+  params <- expand.grid(alpha = alphas, beta = betas)
+  expect_equal(models_df$alpha, params$alpha)
+  expect_equal(models_df$beta, params$beta)
+  ## characteristics
+  expect_equal(models_df$diversity, diversity(models))
+  expect_equal(models_df$iterations, sapply(models, sim_iterations))
+  expect_equal(models_df$converged, sapply(models, sim_converged))
+})
+
+test_that("sim_df modifications remove or keep the sim_df class", {
+  config <- create_locations(25, 20, seed = 48)
+  alphas <- seq(1.25, 2, by = 0.25)
+  betas <- 1 / seq(0.05, 0.5, length.out = 4)
+  models <- grid_blvim(config$costs,
+    config$X,
+    alphas,
+    betas,
+    config$Z,
+    iter_max = 10000,
+    precision = .Machine$double.eps^0.5
+  )
+  models_df <- sim_df(models)
+  names_before <- names(models_df)
+  ## standard modification data.frame style
+  models_df$renyi <- diversity(models, "renyi", 3)
+  expect_s3_class(models_df, "data.frame")
+  expect_s3_class(models_df, "sim_df")
+  expect_equal(models_df$renyi, diversity(models, "renyi", 3))
+  expect_equal(names(models_df), c(names_before, "renyi"))
+  ## extraction without the sim column
+  models_df_nosim <- models_df[c(1:3)]
+  expect_s3_class(models_df_nosim, "data.frame")
+  expect_false(inherits(models_df_nosim, "sim_df"))
+  ## extraction with the sim column
+  models_df_sim <- models_df[c("sim", "alpha")]
+  expect_s3_class(models_df_sim, "data.frame")
+  expect_s3_class(models_df_sim, "sim_df")
+  ## replacing sim by a non sim_list
+  models_df_nosim <- models_df
+  models_df_nosim$sim <- NULL
+  expect_s3_class(models_df_nosim, "data.frame")
+  expect_false(inherits(models_df_nosim, "sim_df"))
+  models_df_nosim <- models_df
+  models_df_nosim$sim <- diversity(models, "renyi", 1.5)
+  expect_s3_class(models_df_nosim, "data.frame")
+  expect_false(inherits(models_df_nosim, "sim_df"))
+  ## replacing sim by a sim_list
+  models_df_sim <- models_df
+  models_df_sim$sim <- models
+  expect_s3_class(models_df_sim, "data.frame")
+  expect_s3_class(models_df_sim, "sim_df")
+})
