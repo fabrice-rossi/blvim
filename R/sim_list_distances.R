e8b68ce1d51df53db158ca0d23ebf4e80761b44c
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-06-11 17:38:44 +0200

    Add a function to compute distances between spatial interaction models.

---
+++ R/sim_list_distances.R
@@ -0,0 +1,55 @@
+#' Compute all pairwise distances between the spatial interaction models in a collection
+#'
+#' This function extracts from each spatial interaction model of the collection a
+#' vector representation derived from its flow matrix (see details). This vector
+#' is then used to compute distances between the models.
+#'
+#' The vector representation is selected using the `flows` parameters. Possible
+#' values are
+#'
+#' -  `"full"` (default value): the representation is obtained by considering the
+#'   matrix of [flows()] as a vector (with the standard [as.vector()] function);
+#' -  `"destination"`: the representation is the [destination_flow()] vector
+#'   associated to each spatial interaction model;
+#' -  `"attractiveness"`: the representation is the [attractiveness()] vector
+#'   associated to each spatial interaction model.
+#'
+#' @param sim_list a collection of spatial interaction models, an object of class
+#'   `sim_list`
+#' @param flows `"full"` (default),  `"destination"` or `"attractiveness"`, see details.
+#' @param method the distance measure to be used. Currently only `"euclidean"` is
+#'  supported
+#' @param ... additional parameters (not used currently)
+#'
+#' @returns an object of class `"dist"`
+#' @seealso [dist()]
+#' @export
+#' @examples
+#' positions <- matrix(rnorm(15 * 2), ncol = 2)
+#' distances <- as.matrix(dist(positions))
+#' production <- rep(1, 15)
+#' attractiveness <- rep(1, 15)
+#' all_flows <- grid_blvim(distances,
+#'   production,
+#'   c(1.1, 1.25, 1.5),
+#'   c(1, 2, 3),
+#'   attractiveness,
+#'   epsilon = 0.1
+#' )
+#' flows_distances <- sim_distance(all_flows)
+#' inflows_distances <- sim_distance(all_flows, "destination")
+sim_distance <- function(sim_list,
+                         flows = c("full", "destination", "attractiveness"),
+                         method = c("euclidean"),
+                         ...) {
+  flows <- rlang::arg_match(flows)
+  method <- rlang::arg_match(method)
+  if (flows == "full") {
+    n_flows <- t(sapply(sim_list, function(x) as.vector(flows(x))))
+  } else if (flows == "destination") {
+    n_flows <- grid_destination_flow(sim_list)
+  } else {
+    n_flows <- grid_attractiveness(sim_list)
+  }
+  collapse::fdist(n_flows)
+}
---
+++ tests/testthat/test-sim_list_distances.R
@@ -0,0 +1,50 @@
+test_that("sim_distance works as expected", {
+  config <- create_locations(20, 30, seed = 16)
+  alphas <- seq(1.25, 2, by = 0.25)
+  betas <- 1 / seq(0.1, 0.5, length.out = 4)
+  models <- grid_blvim(config$costs,
+    config$X,
+    alphas,
+    betas,
+    config$Z,
+    iter_max = 5000,
+    precision = .Machine$double.eps^0.5
+  )
+  withr::local_seed(4)
+  ## full flows
+  m_dist <- as.matrix(sim_distance(models, "full"))
+  from_to <- sample(1:length(models), 8)
+  from <- from_to[1:4]
+  to <- from_to[5:8]
+  for (k in from) {
+    ff_from <- as.vector(flows(models[[k]]))
+    for (l in to) {
+      ff_to <- as.vector(flows(models[[l]]))
+      expect_equal(dist(rbind(ff_from, ff_to))[1], m_dist[k, l])
+    }
+  }
+  ## destination flows
+  m_dist <- as.matrix(sim_distance(models, "destination"))
+  from_to <- sample(1:length(models), 8)
+  from <- from_to[1:4]
+  to <- from_to[5:8]
+  for (k in from) {
+    ff_from <- destination_flow(models[[k]])
+    for (l in to) {
+      ff_to <- destination_flow(models[[l]])
+      expect_equal(dist(rbind(ff_from, ff_to))[1], m_dist[k, l])
+    }
+  }
+  ## full flows
+  m_dist <- as.matrix(sim_distance(models, "attractiveness"))
+  from_to <- sample(1:length(models), 8)
+  from <- from_to[1:4]
+  to <- from_to[5:8]
+  for (k in from) {
+    ff_from <- attractiveness(models[[k]])
+    for (l in to) {
+      ff_to <- attractiveness(models[[l]])
+      expect_equal(dist(rbind(ff_from, ff_to))[1], m_dist[k, l])
+    }
+  }
+})
