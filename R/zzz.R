76173cd0b9326e827f52f9b727befe56ef2ebc37
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-08-28 16:46:31 +0200

    Remove autoplot.sim_list (issue #12).

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   s3_register("ggplot2::autoplot", "sim")
-  s3_register("ggplot2::autoplot", "sim_list")
   s3_register("ggplot2::autoplot", "sim_df")
   invisible()
 }
