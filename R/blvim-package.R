4f69a6c7c3e6ada3d63e8050be700be62fe68b4b
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-09-11 12:13:34 +0200

    Import stats::median

--- R/blvim-package.R
+++ R/blvim-package.R
@@ -1,6 +1,7 @@
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib blvim, .registration = TRUE
+#' @importFrom stats median
 #' @importFrom rlang .data
 ## usethis namespace: end
 NULL
