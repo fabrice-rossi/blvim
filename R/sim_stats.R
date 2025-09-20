196d15a8b6f5337dd6683704fb853b0dd788eb82
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2025-09-20 16:40:20 +0200

    Make median.sim_list return additional information as attributes.

--- R/sim_stats.R
+++ R/sim_stats.R
@@ -1,14 +1,18 @@
-#' Compute the median of a collection of spatial interaction models
+#' Compute the "median" of a collection of spatial interaction models
 #'
 #' This function computes all pairwise distances between spatial interaction
 #' models (SIMs) of its `x` parameter, using `sim_distance()` with the specified
-#' distance parameters. Then it returns the median of the collection defined as
-#' the SIM that is in average the closest to all the other SIMs. Tie breaking
+#' distance parameters. Then it returns the "median" of the collection defined
+#' as the SIM that is in average the closest to all the other SIMs. Tie breaking
 #' uses the order of the SIMs in the collection.
 #'
 #' As distance calculation can be slow in a large collection of SIMs, the
 #' distance object can be returned as a `distances` attribute of the median SIM
-#' by setting the `return_distances` parameter to `TRUE`.
+#' by setting the `return_distances` parameter to `TRUE`. In addition, the
+#' returned SIM has always two attributes:
+#' -  `index` gives the index of the mode in the original `sim_list`
+#' -  `distortion` gives the mean of the distances from the median SIM to all
+#' the other SIMs
 #'
 #' @param x a collection of spatial interaction models, an object of class
 #'   `sim_list`
@@ -19,7 +23,8 @@
 #' @param ... additional parameters (not used currently)
 #'
 #' @inheritParams sim_distance
-#' @returns a spatial interaction model, an object of class `sim`
+#' @returns a spatial interaction model, an object of class `sim` with
+#'   additional attributes
 #' @export
 #'
 #' @examples
@@ -34,7 +39,9 @@
 #'   attractiveness,
 #'   epsilon = 0.1
 #' )
-#' median(all_flows)
+#' all_flows_median <- median(all_flows)
+#' attr(all_flows_median, "index")
+#' attr(all_flows_median, "distortion")
 #' median(all_flows, flows = "destination")
 #' median(all_flows, flows = "attractiveness")
 median.sim_list <- function(x,
@@ -46,10 +53,16 @@ median.sim_list <- function(x,
   if (length(x) > 1) {
     xd <- sim_distance(x, flows, method)
     xdm <- as.matrix(xd)
-    pre <- x[[which.min(colSums(xdm))]]
+    sum_of_dist <- colSums(xdm)
+    best_index <- which.min(sum_of_dist)
+    pre <- x[[best_index]]
+    attr(pre, "index") <- as.integer(best_index)
+    attr(pre, "distortion") <- as.numeric(sum_of_dist[best_index]) / length(x)
   } else {
     pre <- x[[1]]
     xd <- stats::as.dist(0)
+    attr(pre, "index") <- 1L
+    attr(pre, "distortion") <- 0
   }
   if (return_distances) {
     attr(pre, "distances") <- xd
--- tests/testthat/test-sim_stats.R
+++ tests/testthat/test-sim_stats.R
@@ -15,15 +15,26 @@ test_that("median calculation works as expected", {
     expect_s3_class(f_median, "sim")
     ## no distance
     expect_null(attr(f_median, "distances"))
+    ## attributes
+    f_median_pos <- attr(f_median, "index")
+    f_median_distortion <- attr(f_median, "distortion")
+    expect_type(f_median_pos, "integer")
+    expect_type(f_median_distortion, "double")
+    ## remove the attributes for further comparison
+    attr(f_median, "index") <- NULL
+    attr(f_median, "distortion") <- NULL
     ## the median is one of the models
     median_vs_models <- sapply(models, identical, f_median)
     median_idx <- which(median_vs_models)
+    expect_equal(f_median_pos, median_idx)
     ## exactly one match (as objects in models are all different)
     expect_equal(sum(median_vs_models), 1L)
     ## minimal distance
     distances_d <- sim_distance(models, flows = the_flows)
     distances_m <- as.matrix(distances_d)
-    expect_lte(sum(distances_m[median_idx, ]), min(colSums(distances_m)))
+    min_dist <- min(colSums(distances_m))
+    expect_lte(sum(distances_m[median_idx, ]), min_dist)
+    expect_equal(f_median_distortion, min_dist / length(models))
     ## distance saving
     expect_equal(
       attr(median(models, flows = the_flows, return_distances = TRUE), "distances"),
@@ -46,6 +57,10 @@ test_that("median calculation works a single model", {
   )
   for (the_flows in c("full", "destination", "attractiveness")) {
     f_median <- median(models, flows = the_flows)
+    expect_equal(attr(f_median, "index"), 1L)
+    expect_equal(attr(f_median, "distortion"), 0)
+    attr(f_median, "index") <- NULL
+    attr(f_median, "distortion") <- NULL
     expect_equal(f_median, models[[1]])
   }
 })
