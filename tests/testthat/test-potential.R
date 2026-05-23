689a7f902bf6d2b9ec4dad9b9b140b6ed144411b
Fabrice Rossi <Fabrice.Rossi@apiacoa.org>
2026-05-23 15:48:36 +0200

    Add tests of the Jacobian function.

--- tests/testthat/test-potential.R
+++ tests/testthat/test-potential.R
@@ -19,3 +19,25 @@ test_that("potential is correctly computed", {
     expect_equal(sim_potential(model), the_potential)
   }
 })
+
+test_that("Jacobian is NA when it cannot be computed", {
+  config <- create_locations(40, 50, seed = 0)
+  model <- static_blvim(config$costs, config$X, 1.5, 1, config$Z)
+  expect_true(is.na(sim_fp_jacobian(model)))
+})
+
+test_that("Jacobian is correctly computed", {
+  for (k in 1:5) {
+    config <- create_locations(35 + k, 45 + k, seed = k)
+    withr::local_seed(2 * k)
+    kappas <- runif(45 + k, min = 0.5, max = 1.5)
+    model <- blvim(config$costs, config$X, 1.5, 25, config$Z,
+      kappa = kappas,
+      precision = .Machine$double.eps^0.5
+    )
+    expect_equal(sim_fp_jacobian(model),
+      fp_jacobian_non_zero(model),
+      tolerance = 1e-5
+    )
+  }
+})
